3645871a11aa76eb65016385620a1d754ef6020e
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-08-13 05:44:00 -0300

    Cambios en el html

--- public/main.js
+++ public/main.js
@@ -1,4 +1,19 @@
+const API_KEY = '11c16b180032404eb6f65915231308'
+const lat = -34.714428337488094
+const lon = -58.386978067891
 const socket = io();
+
+const fetchData = () =>{
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${lat},${lon}&aqi=yes&lang=es`)
+        .then(response => response.json())
+        .then(data => {
+            const {current:{condition:{text: info_clima }}} = data
+            const {location:{country: pais, name: ciudad, region: provincia}} = data
+            document.getElementById('clima').textContent = `${info_clima}`
+            document.getElementById('localidad').textContent = `${pais}, ${provincia}, ${ciudad}`
+        })
+}
+fetchData()
 
 socket.on('data', data => {
     const { pressure, temperature, humidity, date } = data;

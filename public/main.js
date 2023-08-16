3b3f56dfd66834a8e133097907e6d9c208fdb439
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2023-08-16 18:54:09 -0300

    fix main

--- public/main.js
+++ public/main.js
@@ -18,8 +18,9 @@ fetchData()
 
 socket.on('data', data => {
     const { pressure, temperature, humidity, date } = data;
+    const realTemp = parseInt(temperature)
     document.getElementById('hpa').textContent = `Presión atmosférica: ${pressure} hPa`;
-    document.getElementById('temperature').textContent = temperature, 'C'
+    document.getElementById('temperature').textContent = `${realTemp}°C`
     document.getElementById('humidity').textContent = `Humedad: ${humidity}%`
     document.getElementById('date').textContent = `${date}`
 });
